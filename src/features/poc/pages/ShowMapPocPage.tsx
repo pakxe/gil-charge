fbafe776347b857133bfa3ae4dd9acdedbb7a253
pakxe <pigkill40@naver.com>
2026-08-04 22:17:42 +0900

    feat: ShowMapPocPage에 Polyline 컴포넌트 추가

--- src/features/poc/pages/ShowMapPocPage.tsx
+++ src/features/poc/pages/ShowMapPocPage.tsx
@@ -14,6 +14,7 @@ export function ShowMapPocPage() {
             <Map.CustomOverlay position={INITIAL_POSITION}>
                 <div>안녕</div>
             </Map.CustomOverlay>
+            <Map.Polyline path={[INITIAL_POSITION, { lat: INITIAL_POSITION.lat, lng: INITIAL_POSITION.lng - 4 }]} />
         </Map>
     );
 }
