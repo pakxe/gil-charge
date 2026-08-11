3d1d371a799799d8ebd5dc81a90ef0cb71a27f9c
pakxe <pigkill40@naver.com>
2026-08-11 18:12:41 +0900

    feat: MAP_Z_INDEX에 lasso 추가

--- src/shared/constants/map.ts
+++ src/shared/constants/map.ts
@@ -3,6 +3,7 @@ import { LatLng } from "@/shared/types/map";
 export const DEFAULT_MAP_CENTER: LatLng = { lat: 37.5665, lng: 126.978 };
 
 export const MAP_Z_INDEX = {
+    lasso: 20,
     waypoint: 30,
     selectedWaypoint: 31,
 };
