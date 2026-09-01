84a9636a60b0ca97c45d659f85075345c744f9d1
Pakxe <64801796+pakxe@users.noreply.github.com>
2026-09-01 17:26:11 +0900

    불필요한 파일 제거 (#97)
    * chore: 불필요한 파일 제거
    
    * chore: 불필요한 파일 제거

--- src/shared/model/map.ts
+++ src/shared/model/map.ts
@@ -48,3 +48,23 @@ export type MapInstance = {
     containerPointToLatLng(point: ContainerPoint): LatLng;
     clientPointToLatLng(clientX: number, clientY: number): LatLng;
 };
+
+export interface Station {
+    id: string;
+    name: string;
+    price: number;
+    brandCode: string | null;
+    lat: number;
+    lng: number;
+    localCurrency: {
+        accepted: boolean | null;
+        status: "UNKNOWN" | "ACCEPTED" | "NOT_ACCEPTED" | "OUT_OF_SCOPE" | "MISSING_ROAD_ADDRESS" | "ERROR";
+        roadAddress?: string | null;
+        storeName?: string | null;
+        currencyName?: string | null;
+        industryCode?: string | null;
+    };
+}
+
+export type DrawingType = "pen" | "waypoint";
+export type PathSet = { id: string; type: DrawingType; points: LatLng[] };
